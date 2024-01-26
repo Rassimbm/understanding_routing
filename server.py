7852da39d7709daf4419b97766754aa37dde04f4
Rassim Benmhamed <rassimbenmhamed@outlook.com>
2024-01-26 15:17:46 -0600

    Updated server.py: completed task 3

--- server.py
+++ server.py
@@ -12,6 +12,10 @@ def dojo(name):
     # upper_case = lower_case.capitalize()
     return f"{name.capitalize()}!"
 
+@app.route("/say/<string:name>")
+def say(name):
+    name_output = name.capitalize()
+    return f"Hi {name_output}!"
 
 if __name__ == "__main__":
     app.run(debug=True)
