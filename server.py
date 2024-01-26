b37cfafbc2b57f787168788385603c12c5c4f915
Rassim Benmhamed <rassimbenmhamed@outlook.com>
2024-01-26 14:30:20 -0600

    localhost:5000/dojo - have it say "Dojo!"

--- server.py
+++ server.py
@@ -6,6 +6,12 @@ app = Flask(__name__)
 def hello_world():
     return "Hello World!"
 
+@app.route("/<string:name>")
+def dojo(name):
+    # lower_case = name
+    # upper_case = lower_case.capitalize()
+    return f"{name.capitalize()}!"
+
 
 if __name__ == "__main__":
     app.run(debug=True)
